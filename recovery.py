221b0410e480d431765f48dc1b1f28f8853e1461
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-06-17 22:49:26 -0300

    [FIX] Ajuste na implementação do update

--- recovery.py
+++ recovery.py
@@ -1,33 +1,50 @@
-logDisco = []
+discoLog = []
 memoriaLog = []
-memoriaDado = []
+memoriaDado = [5000,7000,9000,11000,13000]
 discoDado = [5000,7000,9000,11000,13000] 
 #transacao = [] # teste
 
 def commit():
-    y = 0
-    t = input("Digite a transação:")
-    for x in memoriaLog:
-      if t in x:
-        y=+1
-        break
-    logDisco.append(memoriaLog[y])
+    try:
+        y = 0
+        t = input("Digite a transação:")
+        for x in memoriaLog:
+            if t in x:
+                y=+1
+            break
+        discoLog.append(memoriaLog[y])
+    except:
+        print("== Erro ao realizar commit!")
 
 def checkpoint():
-    logDisco = memoriaLog
-    #print(disco)
+    try:
+        discoLog = memoriaLog
+        discoDado = memoriaDado
+        print("Sucesso ao fazer Checkpoint!")
+    except:
+        print("== Erro ao fazer checkpoint!")
 
 def falha():
     memoriaLog.clear() 
     memoriaDado.clear()
 
+def visualizaDiscoDados():
+    print(discoDado)
+
+def visualizaMemoriaDados():
+    print(memoriaDado)
+
 def update():
-    numTransacao = input("Digite a transacao: ")
-    idValor = int(input("ID da pessoa: "))
-    velhoVal = discoDado[idValor - 1]
-    novoVal = int(input("Digite o valor: "))
-    memoriaLog.append((numTransacao,idValor,"salario",velhoVal,novoVal))
-    memoriaDado.append((idValor,novoVal))
+    try:
+        numTransacao = input("Digite a transacao: ")
+        idValor = int(input("ID da pessoa: "))
+        velhoVal = discoDado[idValor - 1]
+        novoVal = int(input("Digite o valor: "))
+        memoriaLog.append((numTransacao,idValor,"salario",velhoVal,novoVal))
+        memoriaDado[idValor - 1] = novoVal
+        print("Sucesso ao fazer Update!")
+    except:
+        print("== Erro ao fazer update!")
 #Transação | ID da pessoa | atributo | valor Antigo | Valor novo
 def menu():
     print("a - Visualizar buffer do Log")
@@ -36,17 +53,19 @@ def menu():
     print("d - Checkpoint")
     print("e - Falha")
     print("f - Commit")
+    print("g - Visualizar dados no disco")
+    print("h - Visualizar dados na memoria")
     print("s - Sair do programa")
     print("Digite a opcao: ")
 
 i = None
 while i != 's':
     menu()
-    i = input()
+    i = input().lower()
     if i == 'a':
         print(memoriaLog)
     elif i == 'b':
-        print(logDisco)
+        print(discoLog)
     elif i == 'c':
         update()
     elif i == 'd':
@@ -56,18 +75,10 @@ while i != 's':
         falha()
     elif i == 'f':
         commit()
+    elif i == 'g':
+        visualizaDiscoDados()
+    elif i == 'h':
+        visualizaMemoriaDados()
     else:
         print("Saindo...")
         break
-    
-    
-
-
-
-
-
-
-
-
-
-
